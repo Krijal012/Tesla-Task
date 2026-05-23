e04382ef073fff0aafaf6a824c89680644d61193
krijal7@gmail.com <krijalshrestha7@gmail.com>
2026-05-23 17:49:25 +0545

    Tesla Cars done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Navbar from './Navbar'
+import Tesla1 from './Tesla1'
+import Tesla2 from './Tesla2'
+import Tesla3 from './Tesla3'
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <Tesla1 />
+      <Tesla2 />
+      <Tesla3 />
     </div>
   )
 }
---
+++ src/Tesla1.jsx
@@ -0,0 +1,31 @@
+export default function Tesla1(){
+    return(
+        <div className="relative">
+            <img 
+                src="../src/assets/tesla_1.jpg"
+                alt="Tesla Picture 1"
+                className="w-full h-auto"
+            />
+            <div className="absolute inset-0 flex flex-col items-center justify-between text-white py-8">
+                {/* Text on top */}
+                <div className="flex flex-col items-center text-black font-serif">
+                    <h1 className="text-6xl font-bold">Tesla Model S</h1>
+                    <h3 className="text-3xl mt-2">
+                        <u>Offer ending September 25</u>
+                    </h3>
+                    <h5 className="text-xl mt-1">From $50000</h5>
+                </div>
+                
+                {/* Buttons on bottom */}
+                <div className="flex gap-4">
+                    <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 cursor-pointer rounded-md font-medium transition-colors">
+                        Order Now
+                    </button>
+                    <button className="bg-white hover:bg-gray-100 text-black px-6 py-2 cursor-pointer rounded-md font-medium transition-colors">
+                        Demo Drive
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
